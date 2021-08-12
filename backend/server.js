6825e5dfde879a9fc8dde1d61df0192a5e807ba2
ganimtron10 <praneethshetty10@gmail.com>
2021-08-12 11:34:42 +0530

    connecting db

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+const uri = process.env.DBURI
+
+const app = express();
+app.use(cors);
+app.use(express.json());
+
+mongoose.connect(uri,{useNewUrlParser: true, useUnifiedTopology: true});
+
+const connection = mongoose.connection;
+connection.once('open', () => {console.log("Connected to db.")})
+
+app.listen(8000, () => {
+	console.log("Server running on the port 8000")
