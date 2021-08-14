3da988c9c3e506fbd809b8b35b696dbc815ab459
akashbhingare <abhingare209@gmail.com>
2021-08-14 11:47:34 +0530

    Teacher Route added

--- backend/route/TeacherRoute.js
+++ backend/route/TeacherRoute.js
@@ -1,2 +1,33 @@
 import express from "express";
+import { boolean } from "yargs";
+import teacherData  from "../models/TeacherDataModel";
+
+const router = express.Router()
+
+router.route('/').get((req,res) => {
+    teacherData.find()
+               .then(teacherdata => res.json(teacherdata))
+               .catch(err => res.status(400).json('Error: '+err))
+})
+
+router.route('/register').post((req,res) => {
+    const firstName = req.body.firstName
+    const middleName = req.body.middleName
+    const lastName = req.body.lastName
+    const dateOfBirth = Date.parse(req.body.dateOfBirth)
+    const gender = Boolean(req.body.gender)
+    const address = req.body.address
+    const mobileNumber = Number(req.body.mobileNumber)
+    const qulification = req.body.qulification
+    const classOfTeaching =req.body.classOfTeaching
+    const subject = req.body.subject
+
+    const teacher = new teacherData({firstName,middleName,lastName,dateOfBirth,gender,
+                                    address,mobileNumber,qulification,classOfTeaching,subject})
+
+    teacher.save()
+           .then(() => res.json('Teacher Added!'))
+           .catch(err => res.status(400).json('Error: '+err))
+    
+})
 
