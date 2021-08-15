6ef8ca71f4cb5978ba95ee422399d7f7db234509
akashbhingare <abhingare209@gmail.com>
2021-08-15 18:31:00 +0530

    Teacher Route Updated

--- backend/route/TeacherRoute.js
+++ backend/route/TeacherRoute.js
@@ -30,4 +30,52 @@ router.route('/register').post((req,res) => {
     
 })
 
+router.route('/:id').get((req,res) => {
+    teacherData.findById(req.params.id)
+               .then(teacherdata => res.json(teacherdata))
+               .catch(err => res.status(400).json('Error: '+err))
+})
+
+router.route('/delete/:id').delete((req,res) => {
+    teacherData.findByIdAndDelete(req.params.id)
+               .then(() => res.json('Teacher Deleted!'))
+               .catch(err => res.status(400).json('Error: '+err))
+})
+
+router.route('/update/:id').post((req,res) => {
+    const firstName = req.body.firstName
+    const middleName = req.body.middleName
+    const lastName = req.body.lastName
+    const dateOfBirth = Date.parse(req.body.dateOfBirth)
+    const gender = Boolean(req.body.gender)
+    const address = req.body.address
+    const mobileNumber = Number(req.body.mobileNumber)
+    const qulification = req.body.qulification
+    const classOfTeaching =req.body.classOfTeaching
+    const subject = req.body.subject
+
+    teacherData.findByIdAndUpdate(req.params.id,
+                                { 
+                                    firstName:firstName,
+                                    middleName:middleName,
+                                    lastName:lastName,
+                                    dateOfBirth:dateOfBirth,
+                                    gender:gender,
+                                    address:address,
+                                    mobileNumber:mobileNumber,
+                                    classOfStuding:classOfStuding,
+                                    divsion:divsion,
+                                    branch:branch
+                                },
+                                (err,docs => {
+                                    if (err){
+                                        res.status(400).json('Error: '+err)
+                                    }
+                                    else{
+                                        console.log('Updated '+docs)
+                                    }
+                                })
+            )
+})
+
 export default router
